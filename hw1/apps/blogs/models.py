006ac0f38acdf1843bb88b1b22489f0a4a6405c8
musulmazhamza <musulmazhamza@gmail.com>
2026-02-14 09:18:01 +0500

    init Models

---
+++ hw1/apps/blogs/models.py
@@ -0,0 +1,47 @@
+from django.db.models import (
+    Model,
+    CharField,
+    SlugField,
+    ForeignKey,
+    CASCADE,
+    TextField,
+    SET_NULL,
+    ManyToManyField,
+    DateTimeField
+)
+
+
+class Category(Model):
+    name = CharField(max_length=100, unique=True)
+    slug = SlugField(max_length=100, unique=True)
+
+
+class Tag(Model):
+    name = CharField(max_length=50, unique=True)
+    slug = SlugField(max_length=100, unique=True)
+
+
+class Post(Model):
+
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+
+    author = ForeignKey('users.User', on_delete=CASCADE)
+    title = CharField(max_length=200)
+    slug = SlugField(max_length=100, unique=True)
+    body = TextField()
+    category = ForeignKey(Category, on_delete=SET_NULL, null=True)
+    tags = ManyToManyField(Tag, blank=True)
+    status = CharField(max_length=20, choices=STATUS_CHOICES)
+    inserted_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+
+class Comment(Model):
+    post = ForeignKey(Post, on_delete=CASCADE)
+    author = ForeignKey('users.User', on_delete=CASCADE)
+    body = TextField()
+    inserted_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
--- hw1/apps/users/models.py
+++ hw1/apps/users/models.py
@@ -1,3 +1,67 @@
-from django.db import models
+from django.db.models import (
+    EmailField,
+    CharField,
+    BooleanField,
+    DateTimeField,
+    ImageField,
+)
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = EmailField(unique=True)
+    first_name = CharField(max_length=50)
+    last_name = CharField(max_length=50)
+
+    is_active = BooleanField(default=True)
+    is_staff = BooleanField(default=False)
+
+    date_joined = DateTimeField(default=timezone.now)
+
+    avatar = ImageField(
+        upload_to="avatars/",
+        blank=True,
+        null=True
+    )
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
